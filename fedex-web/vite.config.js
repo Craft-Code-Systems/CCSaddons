a07d86ff6df4fd3ec015e5adbcc4b6b5bd5e3a09
Elwin Hammer <elwinhammer@gmail.com>
2025-05-04 00:50:04 +0200

    Frist setup & unit tested

---
+++ fedex-web/vite.config.js
@@ -0,0 +1,10 @@
+// vite.config.js
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['tests/**/*.test.ts'],
+    environment: 'node',
+    globals: true
+  }
+});
